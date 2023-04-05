abb6385dd89acf380bf81545ff3db4ef7cea7adf
Daniel Palomo <daniel.palomo@thenetfirm.com>
2023-04-05 16:07:04 +0200

    Pantallas de resultado ok y ko creadas

--- app/src/main/java/com/example/comun/result/ResultKoFragment.java
+++ app/src/main/java/com/example/comun/result/ResultKoFragment.java
@@ -1,4 +1,4 @@
-package com.example.register.steps;
+package com.example.comun;
 
 import android.os.Bundle;
 
@@ -12,10 +12,10 @@ import com.example.cirep_frontend.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Step4Fragment#newInstance} factory method to
+ * Use the {@link ResultKoFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Step4Fragment extends Fragment {
+public class ResultKoFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class Step4Fragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Step4Fragment() {
+    public ResultKoFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class Step4Fragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Step4Fragment.
+     * @return A new instance of fragment ResultKoFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static Step4Fragment newInstance(String param1, String param2) {
-        Step4Fragment fragment = new Step4Fragment();
+    public static ResultKoFragment newInstance(String param1, String param2) {
+        ResultKoFragment fragment = new ResultKoFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class Step4Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_step4, container, false);
+        return inflater.inflate(R.layout.fragment_result_ko, container, false);
     }
 }
---
+++ app/src/main/java/com/example/comun/result/ResultOkFragment.java
@@ -0,0 +1,43 @@
+package com.example.register;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cirep_frontend.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ResultOkFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ResultOkFragment extends Fragment {
+
+
+    public ResultOkFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static ResultOkFragment newInstance(String param1, String param2) {
+        return new ResultOkFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_result_ok, container, false);
+    }
+
+
+}
