bdc18ada5a88c39f9597be9418d1971c03e2dd91
aarongp11 <aarongarciapitarch@gmail.com>
2023-05-09 21:40:49 +0200

    recursos añadidos

---
+++ app/src/main/java/com/example/dashboard/ui/AllIncidencias/AllIncidenciasFragment.java
@@ -0,0 +1,39 @@
+package com.example.dashboard.ui.AllIncidencias;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cirep_frontend.R;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cirep_frontend.R;
+
+
+public class AllIncidenciasFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_all_incidencias, container, false);
+        return view;
+    }
+}
